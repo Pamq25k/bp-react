a046a25201ddffba715eb766c15afd0ecca29118
Pamq25k <billy5210.bt@gmail.com>
2020-07-18 22:48:14 +0100

    Updates

---
+++ precache-manifest.524f449e8782b4c8a8364e036b11ac21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d1e7b0eb1830d344b65cdcb99ee48b",
+    "url": "/react-bp/index.html"
+  },
+  {
+    "revision": "940624a681d64c15b36a",
+    "url": "/react-bp/static/css/main.69626ec6.chunk.css"
+  },
+  {
+    "revision": "e97c43d70fa6d054416f",
+    "url": "/react-bp/static/js/2.55af4fca.chunk.js"
+  },
+  {
+    "revision": "e2b5c50b173a788b8a874a0c4001825d",
+    "url": "/react-bp/static/js/2.55af4fca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "940624a681d64c15b36a",
+    "url": "/react-bp/static/js/main.2f0c497d.chunk.js"
+  },
+  {
+    "revision": "02c0111821a35ed19296",
+    "url": "/react-bp/static/js/runtime-main.d1e4ae11.js"
+  }
+]);
